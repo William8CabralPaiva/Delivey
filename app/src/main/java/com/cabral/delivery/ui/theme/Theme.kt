495fc5d9db44c7ae74a88dc29d665776581cdd32
“William8Cabral” <suporte@vxm.com.br>
2025-09-22 19:58:51 -0300

    theme bars

--- app/src/main/java/com/cabral/delivery/ui/theme/Theme.kt
+++ app/src/main/java/com/cabral/delivery/ui/theme/Theme.kt
@@ -1,11 +1,16 @@
 package com.cabral.delivery.ui.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorPalette = darkColorScheme(
     primary = Indigo400,
@@ -29,13 +34,37 @@ private val LightColorPalette = lightColorScheme(
     onSurface = Color.Black,
     */
 )
-
 @Composable
-fun DeliveryTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun DeliveryTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
         LightColorPalette
+    }
+
+    // Acessa a View local para encontrar a Activity
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        // Usa um SideEffect para mudar as cores das barras
+        SideEffect {
+            val window = (view.context as Activity).window
+            val insetsController = WindowCompat.getInsetsController(window, view)
+
+            // Define a cor da barra de status
+            window.statusBarColor = colors.background.toArgb()
+
+            // Define a cor dos ícones da barra de status
+            insetsController.isAppearanceLightStatusBars = !darkTheme
+
+            // Define a cor da barra de navegação
+            window.navigationBarColor = colors.background.toArgb()
+
+            // Define a cor dos ícones da barra de navegação
+            insetsController.isAppearanceLightNavigationBars = !darkTheme
+        }
     }
 
     MaterialTheme(
